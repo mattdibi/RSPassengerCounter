d4c0eaa3213a736ec7bc8138d8454bbdf339357f
mattdibi <matthewdibi@gmail.com>
2017-03-23 11:09:07 +0100

    Resolved return char bug and improved code

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,10 +12,29 @@ public class Main {
         }
     }
 
+    public static void displayHelp()
+    {
+        System.out.print("*****************************************\n");
+        System.out.print("* COMMAND HELP: \n");
+        System.out.print("* 0 - 5: selecting camera presets\n");
+        System.out.print("* r: resetting counters\n");
+        System.out.print("* c: toggle display calibration\n");
+        System.out.print("* d: toggle display depth view\n");
+        System.out.print("* D: toggle display raw depth view\n");
+        System.out.print("* f: toggle display frame view\n");
+        System.out.print("* s: toggle frame rate stabilization\n");
+        System.out.print("* q: exit program\n");
+        System.out.print("* h: display this help message\n");
+        System.out.print("*****************************************\n");
+
+        return;
+    }
+
     public static void main(String[] args) {
-        char c = 'x';
+        
+        char c;
         boolean stop = false;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner s= new Scanner(System.in);
 
         RSPCN myRSPCN = new RSPCN(0);
         // RSPCN myRSPCN_1 = new RSPCN(1);
@@ -28,48 +48,68 @@ public class Main {
         {
             System.out.print("> ");
 
-            try{
-                c = (char) br.read();
-            } catch (IOException e) {
-                System.out.println("Error reading character!");
-            }
+            c = s.next().charAt(0);
         
             System.out.println("You entered: " + c);
 
-            if(c == 'q')
+            if(c >= '0' && c <= '5')
             {
-                System.out.println("Exiting program.");
-                stop = true;
-                myRSPCN.stop();
-                // myRSPCN_1.stop();
+                System.out.println("Setting camera presets: ");
+                myRSPCN.setCameraPresets((int)(c - '0'));
             }
-            else if( c == 'r')
+            else
             {
-                System.out.println("Resetting counters.");
-                myRSPCN.resetCounters();
-            }
-            else if( c == 'f')
-            {
-                System.out.println("Displaying frame view.");
-                myRSPCN.toggleDisplayFrame();
-            }
-            else if( c == 'c')
-            {
-                System.out.println("Displaying calibration view.");
-                myRSPCN.toggleCalibration();
-            }
-            else if( c == '2')
-            {
-                System.out.println("Changing camera presets.");
-                myRSPCN.setCameraPresets(2);
-            }
-            else if (c == 'd')
-            {
-                System.out.println("Displaying depth view.");
-                myRSPCN.toggleDisplayDepth();
+                switch(c)
+                {
+                    case('h'):
+                        displayHelp();
+                        break;
+
+                    case('r'):
+                        System.out.println("Resetting counters");
+                        myRSPCN.resetCounters();
+                        break;
+                    
+                    case('c'):
+                        System.out.println("Toggle calibration");
+                        myRSPCN.toggleCalibration();
+                        break;
+
+                    case('d'):
+                        System.out.println("Toggle depth view");
+                        myRSPCN.toggleDisplayDepth();;
+                        break;
+
+                    case('D'):
+                        System.out.println("Toggle raw depth view");
+                        myRSPCN.toggleDisplayRawDepth();;
+                        break;
+
+                    case('f'):
+                        System.out.println("Toggle frame view");
+                        myRSPCN.toggleDisplayFrame();
+                        break;
+                    
+                    case('s'):
+                        System.out.println("Toggle frame rate stabilization");
+                        myRSPCN.toggleFrameRateStabilization();
+                        break;
+                    
+                    case('q'):
+                        System.out.println("Exiting program.");
+                        myRSPCN.stop();
+                        // myRSPCN_1.stop();
+                        System.out.println("Capture closed.");
+                        stop = true;
+                        break;
+
+                    default:
+                        displayHelp();
+                        break;
+                }
             }
 
-        }while(!stop);
+        } while(!stop);
 
     }
 }
