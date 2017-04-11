128ad89f79b42e6499682dc3c81378ed2f46eb69
mattdibi <matthewdibi@gmail.com>
2017-04-11 12:31:54 +0200

    Separation of compile and run action

--- java_src/Run.sh
+++ java_src/Run.sh
@@ -1,4 +1,2 @@
-javac -cp jar_files/javacpp.jar:jar_files/librealsense-platform.jar:jar_files/javacv.jar:jar_files/opencv-platform.jar Main.java
 java -jar jar_files/javacpp.jar Main
 java -cp jar_files/javacpp.jar:jar_files/librealsense-platform.jar:jar_files/javacv.jar:jar_files/opencv-platform.jar:. Main
-
