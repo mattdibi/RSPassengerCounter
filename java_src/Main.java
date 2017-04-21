41135ae08d0c1a8f41ac7cbe04d33a1722721e47
mattdibi <matthewdibi@gmail.com>
2017-04-21 11:36:27 +0200

    Added auto device detection

--- java_src/Main.java
+++ java_src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Vector;
 import java.util.Scanner;
 import org.bytedeco.javacpp.RealSense.*;
 
@@ -27,6 +28,7 @@ public class Main {
     public static void main(String[] args) {
 
         context context = new context();
+        Vector<RSPCN> counters = new Vector<RSPCN>(1,1);
 
         char c;
         Scanner s= new Scanner(System.in);
@@ -35,18 +37,12 @@ public class Main {
         int devCount = context.get_device_count();
         System.out.println( "Device count: " + devCount);
 
-        RSPCN myRSPCN = new RSPCN(context, 0);
-        RSPCN mySecondRSPCN = new RSPCN(context, 1);
-
-        System.out.println( "Device 1");
-        System.out.println( "Device name: " + myRSPCN.getDeviceName() );
-        System.out.println( "Device serial: " + myRSPCN.getDeviceSerial() );
-        System.out.println( "Device firmware: " + myRSPCN.getDeviceFirmware() );
-
-        System.out.println( "Device 2");
-        System.out.println( "Device name: " + mySecondRSPCN.getDeviceName() );
-        System.out.println( "Device serial: " + mySecondRSPCN.getDeviceSerial() );
-        System.out.println( "Device firmware: " + mySecondRSPCN.getDeviceFirmware() );
+        for(int i = 0; i < devCount; i++) {
+            counters.add(new RSPCN(context, i));
+            System.out.println( "Device name: " + counters.elementAt(i).getDeviceName() );
+            System.out.println( "Device serial: " + counters.elementAt(i).getDeviceSerial() );
+            System.out.println( "Device firmware: " + counters.elementAt(i).getDeviceFirmware() );
+        }
 
         System.out.println( );
 
@@ -60,27 +56,34 @@ public class Main {
         c = s.next().charAt(0);
 
         if(c == 'M') {
-            myRSPCN.setBareMetalMode(true);
+            for(int i = 0; i < devCount; i++) {
+               counters.elementAt(i).setBareMetalMode(true);
+            }
             System.out.println("Bare metal mode activated");
         }
         else if(c == 'V') {
-            myRSPCN.setVideoRecordMode(true);
+            for(int i = 0; i < devCount; i++) {
+               counters.elementAt(i).setVideoRecordMode(true);
+            }
             System.out.println("Video recording mode activated");
         }
         else if(c == 'B') {
-            myRSPCN.setBareMetalMode(true);
-            myRSPCN.setVideoRecordMode(true);
+            for(int i = 0; i < devCount; i++) {
+               counters.elementAt(i).setBareMetalMode(true);
+               counters.elementAt(i).setVideoRecordMode(true);
+            }
             System.out.println("Video recording in bare metal mode activated");
         }
         else {
             System.out.println("Normal mode activated");
         }
 
-        myRSPCN.start();
-        mySecondRSPCN.start();
+        // Starting all detected cameras
+        for(int i = 0; i < devCount; i++) {
+           counters.elementAt(i).start();
+        }
 
         System.out.println( "Insert command: " );
-
         do {
             System.out.print("> ");
 
@@ -91,70 +94,91 @@ public class Main {
             switch(c) {
                 case('q'): 
                     System.out.println( "Exiting program" );
-                    myRSPCN.stop();
-                    mySecondRSPCN.stop();
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).stop();
+                    }
                     stop = true;
                     break;
 
                 case('r'): 
                     System.out.println( "Resetting counters" );
-                    myRSPCN.resetCounters();
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).resetCounters();
+                    }
                     break;
                 
                 case ('c'): 
                     System.out.println( "Current count: ");
-                    System.out.println("Count in  > " + myRSPCN.getCnt_in());
-                    System.out.println("Count out > " + myRSPCN.getCnt_out());
-                    System.out.println("Current balance > " + (myRSPCN.getCnt_in() - myRSPCN.getCnt_out()));
+                    for(int i = 0; i < devCount; i++) {
+                        System.out.println( );
+                        System.out.println("Device: " + i);
+                        System.out.println("Count in  > " + counters.elementAt(i).getCnt_in());
+                        System.out.println("Count out > " + counters.elementAt(i).getCnt_out());
+                        System.out.println("Current balance > " + (counters.elementAt(i).getCnt_in() - counters.elementAt(i).getCnt_out()));
+                    }
                     break;
                     
                 case ('p'): 
                     System.out.println( "Set preset: ");
                     System.out.print("New preset > ");
                     int preset = s.nextInt();
-                    myRSPCN.setCameraPresets(preset);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setCameraPresets(preset);
+                    }
                     break;
 
                 case ('t'): 
                     System.out.println( "Set threshold: ");
                     System.out.print("New threshold > ");
                     int centimeters = s.nextInt();
-                    myRSPCN.setThresholdCentimeters(centimeters);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setThresholdCentimeters(centimeters);
+                    }
                     break;
                 
                 case ('a') :
                     System.out.println( "Set passenger age: ");
                     System.out.print("New age > ");
                     int age = s.nextInt();
-                    myRSPCN.setMaxPassengerAge(age);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setMaxPassengerAge(age);
+                    }
                     break;
 
                 case ('A') :
                     System.out.println( "Set area threshold: ");
                     System.out.print("New area > ");
                     int area = s.nextInt();
-                    myRSPCN.setAreaThreshold(area);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setAreaThreshold(area);
+                    }
                     break;
                 
                 case ('b') :
                     System.out.println( "Set blur: ");
                     System.out.print("New blur > ");
                     int blur = s.nextInt();
-                    myRSPCN.setBlurSize(blur);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setBlurSize(blur);
+                    }
                     break;
                 
                 case ('x') :
                     System.out.println( "Set xNear: ");
                     System.out.print("New xNear > ");
                     int xNear = s.nextInt();
-                    myRSPCN.setXNear(xNear);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setXNear(xNear);
+                    }
                     break;
                 
                 case ('y') :
                     System.out.println( "Set yNear: ");
                     System.out.print("New yNear > ");
                     int yNear = s.nextInt();
-                    myRSPCN.setYNear(yNear);
+                    for(int i = 0; i < devCount; i++) {
+                       counters.elementAt(i).setYNear(yNear);
+                    }
                     break;
                 
                 default:
