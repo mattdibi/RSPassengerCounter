ace553dbb67bb345cab539807e17098be8237a6b
mattidibi <matthewdibi@gmail.com>
2017-10-03 17:52:45 +0200

    Minor change

--- cpp_src/RSPCN.cpp
+++ cpp_src/RSPCN.cpp
@@ -256,8 +256,7 @@ void RSPCN::count() {
 
                 // -- COUNTING
                 // Up to down
-                if( (passengers[i].getLastPoint().y < frame.rows/2 && passengers[i].getCurrentPoint().y > frame.rows/2) ||
-                    (passengers[i].getLastPoint().y == frame.rows/2 && passengers[i].getCurrentPoint().y > frame.rows/2) ) {
+                if( passengers[i].getLastPoint().y <= frame.rows/2 && passengers[i].getCurrentPoint().y > frame.rows/2 ) {
 
                     cnt_out++;
 
@@ -266,8 +265,7 @@ void RSPCN::count() {
                 }
 
                 // Down to up
-                if( (passengers[i].getLastPoint().y > frame.rows/2 && passengers[i].getCurrentPoint().y < frame.rows/2) ||
-                    (passengers[i].getLastPoint().y == frame.rows/2 && passengers[i].getCurrentPoint().y < frame.rows/2) ) {
+                if( passengers[i].getLastPoint().y >= frame.rows/2 && passengers[i].getCurrentPoint().y < frame.rows/2 ) {
 
                     cnt_in++;
 
